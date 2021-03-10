a633811fe4fc4fd818ec2a0ff77f01df288d5184
Daniel Spence <djhspence@gmail.com>
2021-03-10 10:04:51 -0500

    Tidied up

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
 #include <Arduino.h>
 
-///////////////////////////////////////////////
-///////////////////////////////////////////////
-
 unsigned long checkDelay = 5000;
 unsigned long cycleLength = 3600000;  // 1 hour between checks
 // unsigned long cycleLength = 5000; // 5 secs between checks
@@ -19,35 +16,35 @@ struct Plant {
     int waterValue;
     int threshold;
     int liveRead;
-} black, green, yellow, red;
+}; /*black, green, yellow, red;*/
 
-// Black
-int sens1thresh = commonThresh;
-int relay1 = 2;
-int h2oSensor1 = A1;
-int airValue1 = 572;
-int waterValue1 = 282;
+Plant black = {
+    2,    // relay pin
+    1,    // sensor pin
+    572,  // air reading
+    282   // water reading
+};
 
-// Green
-int sens2thresh = commonThresh;
-int relay2 = 3;
-int h2oSensor2 = A2;
-int airValue2 = 575;
-int waterValue2 = 273;
+Plant green = {
+    3,    // relay pin
+    2,    // sensor pin
+    575,  // air reading
+    273   // water reading
+};
 
-// Yellow
-int sens3thresh = commonThresh;
-int relay3 = 4;
-int h2oSensor3 = A3;
-int airValue3 = 554;
-int waterValue3 = 266;
+Plant yellow = {
+    4,    // relay pin
+    3,    // sensor pin
+    554,  // air reading
+    266   // water reading
+};
 
-// Red
-int sens4thresh = 580;
-int relay4 = 5;
-int h2oSensor4 = A4;
-int airValue4 = 573;
-int waterValue4 = 269;
+Plant red = {
+    5,    // relay pin
+    4,    // sensor pin
+    573,  // air reading
+    269   // water reading
+};
 
 ///////////////////////////////////////////////
 ///////////////////////////////////////////////
@@ -67,73 +64,24 @@ unsigned long plant4Millis = 0;
 
 void setup() {
     Serial.begin(9600);
-    pinMode(relay1, OUTPUT);
-    pinMode(relay2, OUTPUT);
-    pinMode(relay3, OUTPUT);
-    pinMode(relay4, OUTPUT);
-
-    pinMode(h2oSensor1, INPUT);
-    pinMode(h2oSensor2, INPUT);
-    pinMode(h2oSensor3, INPUT);
-    pinMode(h2oSensor4, INPUT);
-
-    digitalWrite(relay1, HIGH);
-    digitalWrite(relay2, HIGH);
-    digitalWrite(relay3, HIGH);
-    digitalWrite(relay4, HIGH);
-    delay(500);
-
     // set up structs for each plant
-    black = {
-        2,    // relay pin
-        1,    // sensor pin
-        572,  // air reading
-        282   // water reading
-    };
 
-    green = {
-        3,    // relay pin
-        2,    // sensor pin
-        575,  // air reading
-        273   // water reading
-    };
+    pinMode(black.relay, OUTPUT);
+    pinMode(green.relay, OUTPUT);
+    pinMode(yellow.relay, OUTPUT);
+    pinMode(red.relay, OUTPUT);
 
-    yellow = {
-        4,    // relay pin
-        3,    // sensor pin
-        554,  // air reading
-        266   // water reading
-    };
-    red = {
-        5,    // relay pin
-        4,    // sensor pin
-        573,  // air reading
-        269   // water reading
-    };
+    pinMode(black.sensor, INPUT);
+    pinMode(green.sensor, INPUT);
+    pinMode(yellow.sensor, INPUT);
+    pinMode(red.sensor, INPUT);
+
+    digitalWrite(black.relay, HIGH);
+    digitalWrite(green.relay, HIGH);
+    digitalWrite(yellow.relay, HIGH);
+    digitalWrite(red.relay, HIGH);
+    delay(500);
 }
-// int h2oSensor;
-
-// plantCheck(h2oSensor) {
-//     unsigned long currentMillis = millis();
-//     if ((currentMillis - plant1Millis) >= cycleLength) {
-//         int h2oSensorReading = analogRead(h2oSensor1);
-//         Serial.print("MOISTURE LEVEL 1:");
-//         Serial.println(h2oSensorReading1);
-//         if (h2oSensorReading1 > 550) {
-//             while (millis() <=
-//                    (currentMillis + 20000)) {  // turn on the water for
-//                    20 secs
-//                 digitalWrite(relay1, LOW);
-//             }
-//             digitalWrite(relay1, HIGH);
-//         } else {
-//             digitalWrite(relay1, HIGH);
-//         }
-
-//         Serial.println();
-//         plant1Millis = currentMillis;
-//     }
-// }
 
 int convertPercent(int value, int min, int max) {
     int moisturePercent = 0;
@@ -153,11 +101,11 @@ void plantCheck(Plant plant) {
             while (millis() <=
                    (currentMillis +
                     waterDuration)) {  // turn on the water for 20 secs
-                digitalWrite(relay1, LOW);
+                digitalWrite(plant.relay, LOW);
             }
-            digitalWrite(relay1, HIGH);
+            digitalWrite(plant.relay, HIGH);
         } else {
-            digitalWrite(relay1, HIGH);
+            digitalWrite(plant.relay, HIGH);
         }
 
         Serial.println();
