ec7bbe9e8f987cee08b14a867e23d68ce53faf70
Daniel Spence <djhspence@gmail.com>
2021-03-15 07:45:37 -0400

    Simplified by only having one main timer rather than a timer inside a timer; made the waterDuration a blocking delay

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,7 @@
 #include <Arduino.h>
 
 const unsigned long checkDelay = 5000;  // time between main loops, non-blocking
-// const unsigned long cycleLength = 3600000; // 1 hour before next potential watering
-// event
-const unsigned long cycleLength = 30000;  // 30 secs between events
+// const unsigned long checkDelay = 3600000; // 1 hour before next potential
 
 const int waterDuration = 2000;  // water for X seconds
 
@@ -16,47 +14,48 @@ struct Plant {
     const int airValue;
     const int waterValue;
     int liveRead;
-    unsigned long plantMillis;
 };
 
-Plant black = {"Black",  // name
-               2,        // relay pin
-               1,        // sensor pin
-               572,      // air reading
-               282,      // water reading
-               0,        // live reading
-               0};       // plant milli counter
+Plant black = {
+    "Black",  // name
+    2,        // relay pin
+    1,        // sensor pin
+    572,      // air reading
+    282,      // water reading
+    0         // live reading
+};
 
-Plant green = {"Green",  // name
-               3,        // relay pin
-               2,        // sensor pin
-               575,      // air reading
-               273,      // water reading
-               0,        // live reading
-               0};       // plant milli counter
+Plant green = {
+    "Green",  // name
+    3,        // relay pin
+    2,        // sensor pin
+    575,      // air reading
+    273,      // water reading
+    0         // live reading
+};
 
-Plant yellow = {"Yellow",  // name
-                4,         // relay pin
-                3,         // sensor pin
-                554,       // air reading
-                266,       // water reading
-                0,         // live reading
-                0};        // plant milli counter
+Plant yellow = {
+    "Yellow",  // name
+    4,         // relay pin
+    3,         // sensor pin
+    554,       // air reading
+    266,       // water reading
+    0          // live reading
+};
 
-Plant red = {"Red",  // name
-             5,      // relay pin
-             4,      // sensor pin
-             573,    // air reading
-             269,    // water reading
-             0,      // live reading
-             0};     // plant milli counter
+Plant red = {
+    "Red",  // name
+    5,      // relay pin
+    4,      // sensor pin
+    573,    // air reading
+    269,    // water reading
+    0       // live reading
+};
 
 ///////////////////////////////////////////////
 ///////////////////////////////////////////////
 
 unsigned long lastCheck = 0;  // set with millis() at the end of each loop
-
-// unsigned long plantMillis = 0;
 
 void setup() {
     Serial.begin(9600);
@@ -76,6 +75,12 @@ void setup() {
     pinMode(yellow.sensor, INPUT);
     pinMode(red.sensor, INPUT);
 
+    // just in case do it again
+    digitalWrite(black.relay, HIGH);
+    digitalWrite(green.relay, HIGH);
+    digitalWrite(yellow.relay, HIGH);
+    digitalWrite(red.relay, HIGH);
+
     delay(500);
 }
 
@@ -86,48 +91,32 @@ int convertPercent(int value, int min, int max) {
 }
 
 void plantCheck(Plant plant) {
-    unsigned long currentMillis = millis();
     plant.liveRead = analogRead(plant.sensor);
     int readingPercent =
         convertPercent(plant.liveRead, plant.airValue, plant.waterValue);
     Serial.print("MOISTURE LEVEL ");
     Serial.print(plant.name);
-
     Serial.print(": ");
     Serial.print(readingPercent);
     Serial.print("%, raw: ");
     Serial.println(plant.liveRead);
-    if (((currentMillis - plant.plantMillis) >= cycleLength) ||
-        lastCheck == 0) {
-        if (readingPercent < percentThresh) {
-            if (millis() <=
-                (currentMillis + waterDuration)) {  // turn on the water
-                // Serial.println("p");
-                Serial.print("Water on relay ");
-                Serial.println(plant.relay);
-                digitalWrite(plant.relay, HIGH);
-            }
-            digitalWrite(plant.relay, LOW);
-            Serial.println("water off");
-        }
-        // else
-        //{
-        digitalWrite(plant.relay, LOW);
-        // Serial.println("do nothing");
-        //}
+    // if (((currentMillis - plant.plantMillis) >= cycleLength) ||
 
-        Serial.println();
-        plant.plantMillis = currentMillis;
-        Serial.print("Black relay state: ");
-        Serial.println(digitalRead(black.relay));
-        Serial.print("Green relay state: ");
-        Serial.println(digitalRead(green.relay));
-        Serial.print("Yellow relay state: ");
-        Serial.println(digitalRead(yellow.relay));
-        Serial.print("Red relay state: ");
-        Serial.println(digitalRead(red.relay));
+    if (readingPercent < percentThresh) {
+        Serial.print("Water on relay ");
+        Serial.println(plant.relay);
+        digitalWrite(plant.relay, LOW);
+        delay(waterDuration);
+        digitalWrite(plant.relay, HIGH);
+        Serial.println("water off");
+    } else {
+        digitalWrite(plant.relay, LOW);
+        Serial.println("do nothing");
     }
+
+
 }
+
 
 void loop() {
     // non-blocking delay loop
@@ -139,7 +128,17 @@ void loop() {
         plantCheck(yellow);
         plantCheck(red);
 
+        Serial.println();
+        Serial.print("Black relay state: ");
+        Serial.println(digitalRead(black.relay));
+        Serial.print("Green relay state: ");
+        Serial.println(digitalRead(green.relay));
+        Serial.print("Yellow relay state: ");
+        Serial.println(digitalRead(yellow.relay));
+        Serial.print("Red relay state: ");
+        Serial.println(digitalRead(red.relay));
         Serial.println("###########");
+        
         lastCheck = millis();
     }
     // end of loop
